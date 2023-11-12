719c6d83332ffbda8671d26be62ab957adc2060e
fruitbox12 <dylanwong007@gmail.com>
2023-11-12 14:25:39 -0800

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
     eslint: {
       ignoreDuringBuilds: true,
     },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:v1*', // This will match all API requests
+          destination: 'http://localhost:3000/api/:v1*', // Proxy to External API
+        },
+      ];
+    },
   };
   
   module.exports = nextConfig;
