c268049ab336de3177a0d69e33654def14037efd
fruitbox12 <dylanwong007@gmail.com>
2023-11-22 08:06:41 -0800

    Update constant.js

--- apps/web/src/store/constant.js
+++ apps/web/src/store/constant.js
@@ -2,7 +2,7 @@
 export const gridSpacing = 3
 export const drawerWidth = 260
 export const appDrawerWidth = 320
-export const baseURL = 'https://9075-3-215-79-139.ngrok-free.app'
+export const baseURL = 'https://bfb250f9ec8d.ngrok.app '
 // process.env.NODE_ENV === 'production' ? window.location.origin : window.location.origin.replace(':8080', ':3000')
 export const NETWORK = {
     MAINNET: 'homestead',
