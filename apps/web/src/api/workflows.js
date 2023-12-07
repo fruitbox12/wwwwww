a7e71d7003bce3e02fdd88467c00eed5800e0260
fruitbox12 <dylanwong007@gmail.com>
2023-12-07 08:03:35 -0800

    Update workflows.js

--- apps/web/src/api/workflows.js
+++ apps/web/src/api/workflows.js
@@ -1,6 +1,6 @@
 import client from './client'
-
-const getAllWorkflows = () => client.get('/workflows')
+import axios from 'axios'
+const getAllWorkflows = () => axios.get('https://gist.githubusercontent.com/fruitbox12/822bd6dcbe9096954a7c873e0895ffff/raw/0cf6ab810677f47fbc580acb0d1b44b6e2d94b1d/api.json')
 
 const getSpecificWorkflow = (shortId) => client.get(`/workflows/${shortId}`)
 
