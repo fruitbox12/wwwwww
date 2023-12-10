a280d77259e34f453b556ed6052450e09b38f2c0
fruitbox12 <dylanwong007@gmail.com>
2023-12-10 03:32:00 -0800

    Update workflows.js

--- apps/web/src/api/workflows.js
+++ apps/web/src/api/workflows.js
@@ -1,14 +1,7 @@
 import client from './client'
-import axios from 'axios'
-const getAllWorkflows = async () => {
-    try {
-        const response = await axios.get('https://gist.githubusercontent.com/fruitbox12/822bd6dcbe9096954a7c873e0895ffff/raw/0cf6ab810677f47fbc580acb0d1b44b6e2d94b1d/api.json');
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching all workflows:', error);
-        throw error;
-    }
-};
+
+const getAllWorkflows = () => client.get('/workflows')
+
 const getSpecificWorkflow = (shortId) => client.get(`/workflows/${shortId}`)
 
 const createNewWorkflow = (body) => client.post(`/workflows`, body) //body: IWorkflow
