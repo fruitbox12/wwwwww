bf3d396821ce85fb4ac9388d979c23b710296cb0
fruitbox12 <dylanwong007@gmail.com>
2023-12-10 07:18:17 -0800

    Update route.ts

--- apps/web/src/app/api/workflowUser/route.ts
+++ apps/web/src/app/api/workflowUser/route.ts
@@ -1,76 +1,57 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { connectMongoDB } from 'libs/database'; // Import the connectMongoDB function
-import Workflow, { IWorkflowDocument } from 'models/workflow'; // Import the Workflow model
-import { ObjectId } from 'mongodb'
+// route.ts
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-interface IWorkflow {
-    _id: ObjectId
-    shortId: string
-    name: string
-    flowData: string
-    deployed: boolean
-    updatedDate: Date
-    createdDate: Date
-}
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
 
-interface IWorkflowResponse extends IWorkflow {
-    execution: IExecution
-    executionCount: number
-}
+  try {
+    const API_KEY = '6uEU4gDFxatldVT39NxpbqRxk7eNGUo2z5R18xhBju2iSEzNl6FxPN1TAwHq86N5'; // Replace with your API key
+    const API_URL = 'https://us-west-2.aws.data.mongodb-api.com/app/data-iwkwk/endpoint/data/v1/action/aggregate';
 
+    const userId = req.query.userId as string; // Get userId from query parameter
 
-export interface IExecution {
-    _id: ObjectId
-    shortId: string
-    workflowShortId: string
-    executionData: string
-    state: ExecutionState
-    createdDate: Date
-    stoppedDate?: Date
-}
-
-interface IWorkflowResponse extends IWorkflowDocument {
-    execution: IExecution[];
-    executionCount: number;
-}
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<IWorkflowResponse[] | { error: string }>) {
-    if (req.method !== 'GET') {
-        return res.status(405).json({ error: 'Method Not Allowed' });
-    }
-
-    try {
-        await connectMongoDB(); // Connect to MongoDB using Mongoose
-
-        const userId = req.query.userId as string; // Assuming you're passing userId as a query parameter
-
-        const workflows: IWorkflowResponse[] = await Workflow.aggregate([
-            {
-                $match: {
-                    userId: userId // Filter workflows by userId
-                }
-            },
-            {
-                $lookup: {
-                    from: 'executions',
-                    localField: 'shortId',
-                    foreignField: 'workflowShortId',
-                    as: 'execution'
-                }
-            },
-            {
-                $addFields: {
-                    executionCount: {
-                        $size: '$execution'
-                    }
-                }
+    const payload = {
+      dataSource: 'Cluster0',
+      database: 'test',
+      collection: 'workflow',
+      pipeline: [
+        {
+          $match: {
+            userId: userId
+          }
+        },
+        {
+          $lookup: {
+            from: 'executions',
+            localField: 'shortId',
+            foreignField: 'workflowShortId',
+            as: 'execution'
+          }
+        },
+        {
+          $addFields: {
+            executionCount: {
+              $size: '$execution'
             }
-        ]).exec();
+          }
+        }
+      ]
+    };
 
-        res.status(200).json(workflows);
-    } catch (error) {
-        console.error('Failed to fetch workflows:', error);
-        res.status(500).json({ error: 'Failed to fetch workflows' });
-    }
+    const response = await axios.post(API_URL, payload, {
+      headers: {
+        'apiKey': API_KEY,
+        'Content-Type': 'application/ejson',
+        'Accept': 'application/json',
+      },
+    });
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Failed to fetch data:', error);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
 }
-
