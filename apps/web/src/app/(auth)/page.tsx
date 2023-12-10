82f5a4d9f7e75acf726d987d5acb92d2a09824e5
fruitbox12 <dylanwong007@gmail.com>
2023-12-10 05:28:26 -0800

    Update page.tsx

--- apps/web/src/app/(auth)/page.tsx
+++ apps/web/src/app/(auth)/page.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import LoginScreen from './login.screen'
-import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
+// pages/login.js (or wherever you want this page to be)
 
-const LoginPage = async () => {
-    const session = await getServerSession()
+import React from 'react';
+import LoginScreen from './LoginScreen'; // Adjust the path as needed
+import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
-    if (session) redirect('/dashboard')
+const LoginPage = () => {
+    const router = useRouter();
 
-    return <LoginScreen />
+    return <LoginScreen />;
+};
+
+export async function getServerSideProps(context) {
+    const session = await getSession({ req: context.req });
+
+    if (session) {
+        return {
+            redirect: {
+                destination: '/dashboard',
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {}, // will be passed to the page component as props
+    };
 }
 
-export default LoginPage
+export default LoginPage;
