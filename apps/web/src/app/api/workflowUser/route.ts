6ef77489a31970e2cbf183a0930a72307b4db3b9
fruitbox12 <dylanwong007@gmail.com>
2023-12-10 06:31:12 -0800

    Update route.ts

--- apps/web/src/app/api/workflowUser/route.ts
+++ apps/web/src/app/api/workflowUser/route.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { connectMongoDB } from 'libs/database'; // Import the connectMongoDB function
+import Workflow, { IWorkflowDocument } from './workflow'; // Import the Workflow model
+import { ObjectId } from 'mongodb'
 
 interface IWorkflow {
     _id: ObjectId
@@ -16,35 +18,54 @@ interface IWorkflowResponse extends IWorkflow {
     executionCount: number
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+export interface IExecution {
+    _id: ObjectId
+    shortId: string
+    workflowShortId: string
+    executionData: string
+    state: ExecutionState
+    createdDate: Date
+    stoppedDate?: Date
+}
+
+interface IWorkflowResponse extends IWorkflowDocument {
+    execution: IExecution[];
+    executionCount: number;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<IWorkflowResponse[] | { error: string }>) {
+    if (req.method !== 'GET') {
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
+
     try {
         await connectMongoDB(); // Connect to MongoDB using Mongoose
 
         const userId = req.query.userId as string; // Assuming you're passing userId as a query parameter
 
-        const workflows: IWorkflowResponse[] = await db.collection('workflow')
-            .aggregate([
-                {
-                    $match: {
-                        userId: userId // Filter workflows by userId
-                    }
-                },
-                {
-                    $lookup: {
-                        from: 'execution',
-                        localField: 'shortId',
-                        foreignField: 'workflowShortId',
-                        as: 'execution'
-                    }
-                },
-                {
-                    $addFields: {
-                        executionCount: {
-                            $size: '$execution'
-                        }
+        const workflows: IWorkflowResponse[] = await Workflow.aggregate([
+            {
+                $match: {
+                    userId: userId // Filter workflows by userId
+                }
+            },
+            {
+                $lookup: {
+                    from: 'executions',
+                    localField: 'shortId',
+                    foreignField: 'workflowShortId',
+                    as: 'execution'
+                }
+            },
+            {
+                $addFields: {
+                    executionCount: {
+                        $size: '$execution'
                     }
                 }
-            ]).toArray();
+            }
+        ]).exec();
 
         res.status(200).json(workflows);
     } catch (error) {
@@ -52,3 +73,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(500).json({ error: 'Failed to fetch workflows' });
     }
 }
+
