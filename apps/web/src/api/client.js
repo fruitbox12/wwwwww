67f9445644a371b0f600133b84bc86f1a131156d
fruitbox12 <dylanwong007@gmail.com>
2023-12-10 03:32:49 -0800

    Update client.js

--- apps/web/src/api/client.js
+++ apps/web/src/api/client.js
@@ -4,8 +4,7 @@ import { baseURL } from 'store/constant'
 const apiClient = axios.create({
     baseURL: `${baseURL}/api/v1`,
     headers: {
-        'Content-type': 'application/json',
-                "ngrok-skip-browser-warning": "69420"
+        'Content-type': 'application/json'
 
     }
 })
